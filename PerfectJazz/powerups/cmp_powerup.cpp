13efcab0fff2f5a715797923742a896d7a9fca06
Carlos Mira <carlosgarciamira@gmail.com>
2021-04-24 11:03:55 +0100

    Added BONUS powerup where coins spell the word BONUS

--- PerfectJazz/powerups/cmp_powerup.cpp
+++ PerfectJazz/powerups/cmp_powerup.cpp
@@ -6,37 +6,12 @@ using namespace std;
 using namespace sf;
 
 void PowerupComponent::deployPowerup() {
-	//probabilities for the columns. Each columns has a chance to be randomly picked
-	std::discrete_distribution<> columnWeights({
-		2,   // 1st column % chance
-		10,  // 2nd column % chance
-		8,   // 3rd column % chance
-		5,   // 4th column % chance
-		8,   // 5th column % chance
-		2,   // 6th column % chance
-		5,   // 7th column % chance
-		10,  // 8th column % chance
-		10,  // 9th column % chance
-		5,   // 10th column % chance
-		2,   // 11th column % chance
-		8,   // 12th column % chance
-		5,   // 13th column % chance
-		8,   // 14th column % chance
-		10,  // 15th column % chance
-		2 });// 16th column % chance
-
-	int choosenColumn = RandomNumber::generateRandomNumber(columnWeights);
-	//cout << choosenColumn << ": colm" << endl;
-
-	//add to x half size of sprite
+	
 	_powerupTextureHelper.spriteTexture.get()->loadFromFile(_powerupTextureHelper.spriteFilename);
 	powerupSpriteCMP = _parent->addComponent<SpriteComponent>();
 	powerupSpriteCMP.get()->loadTexture(_powerupTextureHelper, { 1.f, 1.f }, 0);
 	auto p = _parent->addComponent<BackgroundPhysicsComponent>(Vector2f((float)powerupSpriteCMP->getSprite().getTexture()->getSize().x, (float)powerupSpriteCMP->getSprite().getTexture()->getSize().y));
 	p->setVelocity(Vector2f(0.f, 100.f));
-	//Set the powerup srpite in the right column, cencetered withing the tile
-	_parent->setPosition(Vector2f((round)(mainView.getSize().x / 16 * choosenColumn) + ((mainView.getSize().x / 16) / 2), -200.f));
-	_parent->addTag(_type);
 }
 
 void PowerupComponent::update(double dt) {
@@ -69,6 +44,6 @@ void PowerupComponent::update(double dt) {
 	if (_parent->getPosition().y > _parent->getView().getSize().y) { _parent->setForDelete(); }
 }
 
-PowerupComponent::PowerupComponent(Entity* p, textureSettings powerupTextureHelper, string type) : Component(p), _powerupTextureHelper(powerupTextureHelper), _type(type) {
+PowerupComponent::PowerupComponent(Entity* p, textureSettings powerupTextureHelper) : Component(p), _powerupTextureHelper(powerupTextureHelper) {
 	deployPowerup();
 }
