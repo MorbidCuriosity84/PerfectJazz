c5b4f6cb31af214a77cd7d7502dec7b5c7c0e13d
CarlosMiraGarcia <72560934+CarlosMiraGarcia@users.noreply.github.com>
2021-04-29 12:02:12 +0100

    Updating missile changes

--- PerfectJazz/movement/cmp_missile_movement.cpp
+++ PerfectJazz/movement/cmp_missile_movement.cpp
@@ -9,6 +9,7 @@
 using namespace Physics;
 
 void MissileMovementComponent::update(double dt) {
+	
 	if (_seeking) {
 
 		if (player != NULL && player->isAlive()) {
@@ -55,31 +56,6 @@ void MissileMovementComponent::setPhysics(shared_ptr<PhysicsComponent> phys) { _
 
 shared_ptr<PhysicsComponent> MissileMovementComponent::getPhysics() const { return _parentPhysics; }
 
-<<<<<<< Updated upstream
 MissileMovementComponent::MissileMovementComponent(Entity* p, sf::Vector2f vel, bool seek, _entityCategory cat) : MovementComponent(p, vel), _seeking(seek), cat(cat) {	
 	_parentSprite = _parent->GetCompatibleComponent<SpriteComponent>()[0];
 }
-=======
-MissileMovementComponent::MissileMovementComponent(Entity* p, sf::Vector2f vel, bool seek, _entityCategory cat) : MovementComponent(p, vel), _seeking(seek), cat(cat), contactCount(0) {
-	_parentPhysics = _parent->GetCompatibleComponent<PhysicsComponent>()[0];
-	_parentSprite = _parent->GetCompatibleComponent<SpriteComponent>()[0];
-	b2FixtureDef missileRadar;
-	b2CircleShape circleShape;
-	auto shape = _parent->addComponent<ShapeComponent>();
-	shape->setShape<CircleShape>(240.f);
-	shape->getShape().setOrigin({ 240.f,240.f });
-	shape->getShape().setFillColor(sf::Color(0, 0, 0, 45));
-	circleShape.m_radius = 8;
-	missileRadar.shape = &circleShape;
-	missileRadar.isSensor = true;
-	if (cat == ENEMY_MISSILE) {
-		missileRadar.filter.categoryBits = ENEMY_MISSILE_RADAR;
-		missileRadar.filter.maskBits = PLAYER_BODY;
-	}
-	else {
-		missileRadar.filter.categoryBits = FRIENDLY_MISSILE_RADAR;
-		missileRadar.filter.maskBits = ENEMY_BODY;
-	}
-	_parentPhysics.get()->getBody()->CreateFixture(&missileRadar);
-}
->>>>>>> Stashed changes
